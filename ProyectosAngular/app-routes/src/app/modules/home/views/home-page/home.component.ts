d87c1ac6d47151b22f75748aa5ebf36d9f07609e
S-KiRa <skira250128@gmail.com>
2024-09-21 10:57:24 -0500

    Gabriel Albán 20240921-1057

--- ProyectosAngular/app-routes/src/app/modules/home/views/home-page/home.component.ts
+++ ProyectosAngular/app-routes/src/app/modules/home/views/home-page/home.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { SidebarPageComponent } from "../../../sidebar/views/sidebar-page/sidebar.component";
+import { AuthPageComponent } from "../../../auth/views/auth-page/auth.component";
+import { ContentPageComponent } from "../../../content/views/content-page/content-page.component";
+import { SidetopPageComponent } from "../../../sidetop/views/sidetop-page/sidetop-page.component";
+import { PrimePageComponent } from "../../../UI/views/prime-page/prime-page.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [SidebarPageComponent, AuthPageComponent, ContentPageComponent, 
+            SidetopPageComponent, PrimePageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
